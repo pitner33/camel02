46936e0cafb075e56653c9fca7cc5ca5fd7e617c
pitner33 <pitner33@gmail.com>
2019-06-07 11:08:32 +0200

    (add) xml routes with processor added

--- src/main/java/com/example/resttofile/GreetingProcessor.java
+++ src/main/java/com/example/resttofile/GreetingProcessor.java
@@ -2,7 +2,9 @@ package com.example.resttofile;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GreetingProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
